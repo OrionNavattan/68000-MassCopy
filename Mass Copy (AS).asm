d58c508160613f358569df21a45ff7a85b51d89d
OrionNavattan <103385399+OrionNavattan@users.noreply.github.com>
2024-05-18 16:14:58 -0700

    Add files and populate readme

---
+++ Mass Copy (AS).asm
@@ -0,0 +1,170 @@
+srcreg:		reg a0
+destreg:	reg a1
+	outradix	10	; use decimal numbers when converting integers to strings
+
+; -------------------------------------------------------------------------
+; Unrolled loop to perform a mass copy of up to 1 kilobyte of data via
+; longword moves. Can be called for any amount of data between 4 and 1024
+; bytes by placing the number of bytes after "MassCopy_";
+;  e.g., "jsr MassCopy_128".
+
+; If the data size is larger than 1 KB or is not divisible by a longword,
+; use the "MassCopy" subroutine below.
+
+; input:
+;	srcreg.l = source
+;	destreg.l = destination
+; -------------------------------------------------------------------------
+
+genmasscopy:	macro
+
+lblnum		:= "\{c}"			; number used in label
+
+MassCopy_{lblnum}:	label *
+	move.l	(srcreg)+,(destreg)+
+c := c-4					; decrement label number
+	endm
+
+c := 1024
+
+	rept c/4
+	genmasscopy
+	endm
+
+MassCopy_Base:	; used for dynamic calls into the above
+MassCopy_Done:
+	rts
+
+; -------------------------------------------------------------------------
+; Subroutine to perform a mass copy of up to $3FFFF of data.
+; Sets ups/manages calls to the unrolled loop, automatically switching to
+; byte moves if one or both of source or destination are odd, and dealing
+; with any remainder if longword moves are used.
+
+; input:
+;	srcreg.l = source
+;	destreg.l = destination
+;	d0.l = size of data to copy in bytes
+
+; example usage, assuming source and destination are a0 and a1:
+;	lea source(pc),a0
+;	lea (destination).w,a1
+;	jsr	(MassCopy).l
+
+; uses d1.l (byte moves only), d3.w, d4.l, d5.l
+; -------------------------------------------------------------------------
+
+MassCopy:
+	tst.l	d0
+	beq.s	MassCopy_Done				; exit if size is 0
+
+	move.l	srcreg,d4
+	move.l	destreg,d5
+	sub.l	d4,d5
+	bpl.s	.positive
+	neg.l	d5
+
+.positive:
+	cmpi.l	#4,d5				; d5 = difference between source and destination addresses
+	bcs.w	MassCopy_Byte		; fall back to bytes if less than 4 (a possibility with RLE compression algorithms)
+
+	move.w	destreg,d5
+	moveq	#1,d3					; faster and smaller than using two 'andi #1's
+	and.w	d3,d4					; d4 = 0 if source is even; 1 if odd
+	and.w	d3,d5					; d5 = same as above, but for destination
+	eor.w	d4,d5					; are source and destination both even or both odd?
+	bne.w	MassCopy_Byte				; branch if not (fall back to bytes)
+
+	tst.b	d4					; are source and destination even?
+	beq.s	.even					; branch if so
+
+	move.b	(srcreg)+,(destreg)+			; copy one byte to align source and destination to even
+	subq.l	#1,d0					; minus 1 byte copied
+
+.even:
+	move.l	d0,d5					; back up total size for later
+	lsr.l	#2,d0					; d0 = total count of longwords to copy (divide total bytes by four)
+	beq.w	MassCopy_FinishBytes			; branch if fewer than 4 bytes total
+
+	move.w	d0,d4					; back up total longwords for later
+	lsr.w	#8,d0					; d0 = count of whole kilobytes (divide total longwords by 256)
+	beq.s	.less_than_1kb				; branch if less than 1 kilobyte total
+	subq.w	#1,d0					; adjust for loop counter
+
+.longwordloop:
+	bsr.w	MassCopy_1024				; copy 1 kilobyte
+	dbf	d0,.longwordloop			; repeat for all whole kilobytes
+
+	andi.w	#$FF,d4					; d4 = remaining longwords to copy
+	beq.s	.nolongremainder			; branch if 0 (0-4 bytes leftover)
+
+.less_than_1kb:
+	neg.w	d4					; invert remaining longword count
+	add.w	d4,d4					; multiply by 2 (size of 'move.l (srcreg)+,(destreg)+') to make index
+	jsr	MassCopy_Base(pc,d4.w)			; jump to appropriate location in unrolled loop to copy remaining longwords
+
+.nolongremainder:
+	andi.l	#3,d5					; d5 = remainder if data size was not divisible by a longword
+	bne.w	MassCopy_FinishBytes			; do any leftover bytes if necessary
+	rts
+
+; -------------------------------------------------------------------------
+; Unrolled loop to perform a mass copy of up to 512 bytes of data via
+; byte moves. While this can be used the same way as the MassCopy loop, it
+; is recommended to call MassCopy instead as that will automatically
+; optimize the operation to longword moves if the source and destination
+; addresses are even. You MUST use MassCopy if you have more than 512 bytes
+; to copy.
+
+; input:
+;	srcreg.l = source
+;	destreg.l = destination
+; -------------------------------------------------------------------------
+
+genmasscopyb:	macro
+
+lblnum		:= "\{c}"			; number used in label
+
+MassCopyByte_{lblnum}:	label *
+	move.l	(srcreg)+,(destreg)+
+c := c-4					; decrement label number
+	endm
+
+c := 512
+
+	rept c
+	genmasscopyb
+	endm
+
+MassCopyByte_Base:						; used for dynamic calls into the above
+MassCopyByte_Done:
+	rts
+
+; -------------------------------------------------------------------------
+; Similar to MassCopy, expect byte-length moves are used
+
+; uses d1.l, d5.l
+; -------------------------------------------------------------------------
+
+MassCopy_Byte:
+	move.l	d0,d5					; back up total byte count for later
+	beq.s	MassCopyByte_Done			; exit if size is 0
+	moveq	#9,d1					; shift by 9 to divide by 512
+	lsr.l	d1,d0					; d0 = count of half-kilobytes (512 bytes)
+	beq.s	MassCopy_FinishBytes			; branch if fewer than 512 bytes
+
+	subq.w	#1,d0					; adjust for loop counter
+
+.loop512:
+	bsr.w	MassCopyByte_512			; copy 512 bytes
+	dbf	d0,.loop512				; repeat for all half-kilobytes
+
+	andi.l	#$FF,d5					; d5 = count of remaining bytes
+	beq.s	MassCopyByte_Done			; branch if no remainder
+
+MassCopy_FinishBytes:
+	neg.w	d5					; invert count of remaining bytes
+	add.w	d5,d5					; multiply by 2 (size of 'move.b (srcreg)+,(destreg)+') to make index
+	jmp	MassCopyByte_Base(pc,d5.w)		; jump to appropriate location in unrolled loop to copy remaining bytes
+
+	outradix 16		; restore default outradix
--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # 68000 MassCopy
- Unrolled-loop mass copy for 68000-based platforms
+
+ Unrolled-loop mass copy library for 68000-based platforms, targeting the ASM68K and Macro Assembler AS assemblers.
+Supports up to $3FFFF bytes, with no restrictions on alignment or location.
+
+This library combines a pair of unrolled loops, one copying $400 (1024) bytes via longword moves, 
+the other $200 (512) bytes via byte moves, with a couple of routines to manage copying arbitrary amounts of data.
+In order to speed up the process, the code will favor longword moves for the operation, only falling back to byte moves
+if the source and destination addresses cannot be aligned to even (if both are odd aligned, a single byte will be copied to align to even).
+To allow more flexibility of use, the unrolled loops have labels within them to enable direct calls into the loops themselves for
+data copies with fixed amounts (see usage examples below). For ease of customization, register equates are used for the source 
+and destination address registers.
+
+## Usage examples
+
+The following show a couple different ways the library can be used. Note that for small amounts, this routine will be slower than using
+a dbf loop, though gains may be realized nonetheless from odd source and destinations being shifted to even.
+
+#### Copying dictionary matches in MDComp Kosinski Decompressor (a2 = copy source, a1 = copy destination)
+
+```
+	; Short matches
+.streamcopy:
+	adda.w	d5,a2		; a2 = start of match
+	addq.b	#2,d4		; d4 = actual size of data, no more than 5 bytes
+	moveq	#0,d5
+	move.b	d4,d5
+	jsr MassCopy_FinishBytes	; use value in d5 to make direct jump into byte copy loop
+	bra.w	.fetchnewcode
+; =============================
+
+	; Long matches
+	move.b	(a0)+,d4				; d4 - count
+	beq.s	.quit					; if 0, we are done
+	cmpi.b	#1,d4
+	beq.w	.fetchnewcode			; if 1, fetch a new code.
+
+	adda.w	d5,a2					; a2 = start of match
+	addq.w	#1,d4					; d4 = actual size of data to copy
+		
+	movem.l	d0/d1/d3,-(sp)				; back up descriptor fields and switch flag
+	move.w	d4,d0
+	jsr MassCopy
+	movem.l	(sp)+,d0/d1/d3
+	bra.w	.fetchnewcode
+```
+
+
+#### Copying a rendered Sega CD GFX of 22 KB ($5800 bytes) (a0 = source, a1 = destination)
+
+```
+.copyrender:
+	lea (v_rendered_frame).l,a0	; Sega CD wordram
+	lea (v_frame_buffer).w,a1	; Genesis workram
+	
+	rept	$5800/$400
+	jsr	MassCopy_1024	; call the full loop 22 times
+	endr
+		
+```
+
